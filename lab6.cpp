df76ebd6af5c7b637e86f232a11b1c474fb22acc
timursurin <timursur58@gmail.com>
2024-12-15 19:14:42 +0300

    lab3

--- lab6.cpp
+++ lab6.cpp
@@ -5,25 +5,24 @@ using namespace std;
 
 class Solution {
 public:
-    int maxSatisfaction(vector<int>& satisfaction) {
-        sort(satisfaction.rbegin(), satisfaction.rend());
-        int maxSatisfaction = 0;
-        int currentSum = 0;
-        int totalSum = 0;
-        for (int i = 0; i < satisfaction.size(); ++i) {
+    int maxSatisfaction(vector<int>& satisfaction) { // 4 * n байт
+        sort(satisfaction.rbegin(), satisfaction.rend()); // ~8 * log2(n) байт.
+        int maxSatisfaction = 0; // 4 байта
+        int currentSum = 0;      // 4 байта
+        int totalSum = 0;        // 4 байта
+        for (int i = 0; i < satisfaction.size(); ++i) { // O(n)
             currentSum += satisfaction[i];
             if (currentSum < 0) break;
             totalSum += currentSum;
         }
-
         return totalSum;
     }
 };
 
 int main() {
-    vector<int> satisfaction = {-1, -8, 0, 2, 3, 8, -15};
+    vector<int> satisfaction = {-1, -8, 0, 2, 3, 8, -15}; // (4 * n) + 16 (служ данные) = 44 байта.
     Solution solution;
-    int result = solution.maxSatisfaction(satisfaction);
+    int result = solution.maxSatisfaction(satisfaction); // 4 * n байт.
     cout << result << endl;
     return 0;
 }
